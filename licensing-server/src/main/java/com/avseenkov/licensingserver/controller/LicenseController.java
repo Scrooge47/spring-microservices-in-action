28a647c456056635887dcc31b390b133e8ef40c5
Alex Avseenkov <a.avseenkov@yandex.ru>
2023-08-30 10:59:25 +0300

    add config server

--- licensing-server/src/main/java/com/avseenkov/licensingserver/controller/LicenseController.java
+++ licensing-server/src/main/java/com/avseenkov/licensingserver/controller/LicenseController.java
@@ -18,53 +18,33 @@ public class LicenseController {
 
     private LicenseService licenseService;
 
-    @GetMapping("/{licenseId}")
-    public ResponseEntity<License> getLicense(
-            @PathVariable String organizationId,
-            @PathVariable String licenseId
-    ) {
-        License license = licenseService
-                .getLicense(licenseId, organizationId);
+    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
+    public ResponseEntity<License> getLicense( @PathVariable("organizationId") String organizationId,
+                                               @PathVariable("licenseId") String licenseId) {
 
-        license.add(linkTo(methodOn(LicenseController.class)
-                        .getLicense(organizationId, license.getLicenseId()))
-                        .withSelfRel(),
-                linkTo(methodOn(LicenseController.class)
-                        .createLicense(organizationId, license, null))
-                        .withRel("createLicense"),
-                linkTo(methodOn(LicenseController.class)
-                        .updateLicense(organizationId, license))
-                        .withRel("updateLicense"),
-                linkTo(methodOn(LicenseController.class)
-                        .deleteLicense(organizationId, license.getLicenseId()))
-                        .withRel("deleteLicense"));
+        License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(
+                linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
+                linkTo(methodOn(LicenseController.class).createLicense(license)).withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class).updateLicense(license)).withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class).deleteLicense(license.getLicenseId())).withRel("deleteLicense")
+        );
+
         return ResponseEntity.ok(license);
     }
 
     @PutMapping
-    public ResponseEntity<String> updateLicense(
-            @PathVariable("organizationId")
-            String organizationId,
-            @RequestBody License request) {
-        return ResponseEntity.ok(licenseService.updateLicense(request,
-                organizationId));
+    public ResponseEntity<License> updateLicense(@RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request));
     }
 
     @PostMapping
-    public ResponseEntity<String> createLicense(
-            @PathVariable("organizationId") String organizationId,
-            @RequestBody License request,
-            @RequestHeader(value = "Accept-Language", required = false) Locale locale
-    ) {
-        return ResponseEntity.ok(licenseService.createLicense(request,
-                organizationId, locale));
+    public ResponseEntity<License> createLicense(@RequestBody License request) {
+        return ResponseEntity.ok(licenseService.createLicense(request));
     }
 
-    @DeleteMapping(value = "/{licenseId}")
-    public ResponseEntity<String> deleteLicense(
-            @PathVariable("organizationId") String organizationId,
-            @PathVariable("licenseId") String licenseId) {
-        return ResponseEntity.ok(licenseService.deleteLicense(licenseId,
-                organizationId));
+    @DeleteMapping(value="/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
     }
 }
