a26e5b37f31a4732cb5a70d01b6320428aa8d1a4
Alex Avseenkov <a.avseenkov@yandex.ru>
2023-10-11 10:44:25 +0300

    add discovery server

---
+++ organization-service/src/test/java/com/avseenkov/organization/OrganizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avseenkov.organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
